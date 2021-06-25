35533e3890f57a4f25d9e1a5b9776af2fb6d839e
JoJo Wang <id233233@outlook.com>
2021-06-25 12:34:01 +0800

    fix: rename DTOBoolean -> DTOBool

--- util/util-guava-ext/src/main/java/io/github/jojoti/utilguavaext/DTOBool.java
+++ util/util-guava-ext/src/main/java/io/github/jojoti/utilguavaext/DTOBool.java
@@ -4,22 +4,22 @@ package io.github.jojoti.utilguavaext;
  * @author JoJo Wang
  * @link github.com/jojoti
  */
-public class DTOBoolean<T> {
+public class DTOBool<T> {
 
     private final boolean success;
     private final T data;
 
-    private DTOBoolean(boolean success, T data) {
+    private DTOBool(boolean success, T data) {
         this.success = success;
         this.data = data;
     }
 
-    public static <T> DTOBoolean<T> ok(T t) {
-        return new DTOBoolean<>(true, t);
+    public static <T> DTOBool<T> ok(T t) {
+        return new DTOBool<>(true, t);
     }
 
-    public static <T> DTOBoolean<T> failed(T t) {
-        return new DTOBoolean<>(false, t);
+    public static <T> DTOBool<T> failed(T t) {
+        return new DTOBool<>(false, t);
     }
 
     public boolean isSuccess() {
