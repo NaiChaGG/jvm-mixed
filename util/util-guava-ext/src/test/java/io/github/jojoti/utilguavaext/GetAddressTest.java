dfabad0ecf5c5a7e5a7e17890be0b51d5bcbfa13
JoJo Wang <id233233@outlook.com>
2021-07-25 19:43:24 +0800

    fix: dns uri 测试

--- util/util-guava-ext/src/test/java/io/github/jojoti/utilguavaext/GetAddressTest.java
+++ util/util-guava-ext/src/test/java/io/github/jojoti/utilguavaext/GetAddressTest.java
@@ -1,5 +1,6 @@
 package io.github.jojoti.utilguavaext;
 
+import com.google.common.net.InetAddresses;
 import org.junit.jupiter.api.Test;
 
 /**
@@ -9,6 +10,14 @@ class GetAddressTest {
 
     @Test
     void getSocketAddress() {
+        var rs = InetAddresses.isUriInetAddress("dns:///foo.googleapis.com:8080");
+        System.out.println(rs);
+        var rs1 = InetAddresses.isUriInetAddress("dns:///foo.googleapis.com:8080");
+        System.out.println(rs1);
+        var rs2 = InetAddresses.isUriInetAddress("zookeeper://zk.example.com:9900/example_service");
+        System.out.println(rs2);
+
+
         var sout = GetAddress.getSocketAddress("doamin:9000");
     }
 
